0af278a42c8815d9775ff7603fe81def5bed94fa
mr. Alexandr Ryabokon <mr.alexandr.ryabokon@gmail.com>
2017-08-18 18:05:52 +0300

    Import code from ohdsi/webapi
    * CohortExpressionQueryBuilder
    * Dependency optimization
    * changed version format

---
+++ src/main/java/org/ohdsi/circe/cohortdefinition/DeviceExposure.java
@@ -0,0 +1,71 @@
+/*
+ *
+ * Copyright 2017 Observational Health Data Sciences and Informatics
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Authors: Christopher Knoll
+ *
+ */
+package org.ohdsi.circe.cohortdefinition;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.ohdsi.circe.vocabulary.Concept;
+
+/**
+ *
+ * @author cknoll1
+ */
+public class DeviceExposure extends Criteria {
+
+  @JsonProperty("CodesetId")
+  public Integer codesetId;
+
+  @JsonProperty("First")
+  public Boolean first;
+
+  @JsonProperty("OccurrenceStartDate")
+  public DateRange occurrenceStartDate;
+
+  @JsonProperty("OccurrenceEndDate")
+  public DateRange occurrenceEndDate;
+
+  @JsonProperty("DeviceType")
+  public Concept[] deviceType;
+
+  @JsonProperty("UniqueDeviceId")
+  public TextFilter uniqueDeviceId;
+
+  @JsonProperty("Quantity")
+  public NumericRange quantity;
+
+  @JsonProperty("DeviceSourceConcept")
+  public Integer deviceSourceConcept;
+
+  @JsonProperty("Age")
+  public NumericRange age;
+
+  @JsonProperty("Gender")
+  public Concept[] gender;
+
+  @JsonProperty("ProviderSpecialty")
+  public Concept[] providerSpecialty;
+
+  @JsonProperty("VisitType")
+  public Concept[] visitType;
+
+  @Override
+  public String accept(IGetCriteriaSqlDispatcher dispatcher) {
+    return dispatcher.getCriteriaSql(this);
+  }
+
+}
---
+++ src/main/java/org/ohdsi/circe/vocabulary/Concept.java
@@ -0,0 +1,90 @@
+/*
+ *
+ * Copyright 2017 Observational Health Data Sciences and Informatics
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Authors: Christopher Knoll
+ *
+ */
+package org.ohdsi.circe.vocabulary;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ *
+ * @author fdefalco
+ */
+@JsonInclude(Include.ALWAYS)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Concept {
+    @JsonProperty("CONCEPT_ID")
+    public Long conceptId;
+    
+    @JsonProperty("CONCEPT_NAME")
+    public String conceptName;
+    
+    @JsonProperty("STANDARD_CONCEPT_CAPTION")
+    public String GetStandardConcept() {
+      if (standardConcept == null)
+        return "Unknown";
+      
+      switch (standardConcept) {
+        case "N" : 
+          return "Non-Standard";
+        case "S" :
+          return "Standard";
+        case "C" :
+          return "Classification";
+        default:
+          return "Unknown";
+      }
+    }
+    
+    @JsonProperty("STANDARD_CONCEPT")
+    public String standardConcept;
+    
+    @JsonProperty("INVALID_REASON_CAPTION")
+    public String GetInvalidReason() {
+      if (invalidReason == null)
+        return "Unknown";
+      
+      switch (invalidReason) {
+        case "V" : 
+          return "Valid";
+        case "D" :
+          return "Invalid";
+        case "U" :
+            return "Invalid";
+        default:
+          return "Unknown";
+      }
+    }
+    
+    @JsonProperty("INVALID_REASON")
+    public String invalidReason;
+    
+    @JsonProperty("CONCEPT_CODE")
+    public String conceptCode;
+    
+    @JsonProperty("DOMAIN_ID")
+    public String domainId;
+    
+    @JsonProperty("VOCABULARY_ID")
+    public String vocabularyId;
+    
+    @JsonProperty("CONCEPT_CLASS_ID")
+    public String conceptClassId;
+}
