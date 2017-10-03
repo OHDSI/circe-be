91f4d6b99185da00dbe55602e0fda2e57b34298f
Chris Knoll <cknoll@ohdsi.org>
2017-10-03 12:51:44 -0400

    Revert dense_rank() to row_number() to enforce one event per person.
    Updated library to v1.2.1.
    
    Fixes #9.

--- src/main/resources/resources/cohortdefinition/sql/conditionEra.sql
+++ src/main/resources/resources/cohortdefinition/sql/conditionEra.sql
@@ -2,7 +2,7 @@
 select C.person_id, C.condition_era_id as event_id, C.condition_era_start_date as start_date, C.condition_era_end_date as end_date, C.CONDITION_CONCEPT_ID as TARGET_CONCEPT_ID, NULL as visit_occurrence_id
 from 
 (
-  select ce.*, dense_rank() over (PARTITION BY ce.person_id ORDER BY ce.condition_era_start_date, ce.condition_era_id) as ordinal
+  select ce.*, row_number() over (PARTITION BY ce.person_id ORDER BY ce.condition_era_start_date, ce.condition_era_id) as ordinal
   FROM @cdm_database_schema.CONDITION_ERA ce
 @codesetClause
 ) C
--- src/main/resources/resources/cohortdefinition/sql/measurement.sql
+++ src/main/resources/resources/cohortdefinition/sql/measurement.sql
@@ -2,7 +2,7 @@
 select C.person_id, C.measurement_id as event_id, C.measurement_date as start_date, DATEADD(d,1,C.measurement_date) as END_DATE, C.measurement_concept_id as TARGET_CONCEPT_ID, C.visit_occurrence_id
 from 
 (
-  select m.*, dense_rank() over (PARTITION BY m.person_id ORDER BY m.measurement_date, m.measurement_id) as ordinal
+  select m.*, row_number() over (PARTITION BY m.person_id ORDER BY m.measurement_date, m.measurement_id) as ordinal
   FROM @cdm_database_schema.MEASUREMENT m
 @codesetClause
 ) C
