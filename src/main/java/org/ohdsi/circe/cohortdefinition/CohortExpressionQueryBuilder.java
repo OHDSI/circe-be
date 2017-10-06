259b1beadc5186f75a608865fdcb04472ffab9c6
raomane <32525175+raomane@users.noreply.github.com>
2017-10-06 07:27:43 -0400

    Update CohortExpressionQueryBuilder.java

--- src/main/java/org/ohdsi/circe/cohortdefinition/CohortExpressionQueryBuilder.java
+++ src/main/java/org/ohdsi/circe/cohortdefinition/CohortExpressionQueryBuilder.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * Authors: Chris Knoll
+ * Authors: Chris Knoll, Gowtham Rao
  *
  */
 package org.ohdsi.circe.cohortdefinition;
