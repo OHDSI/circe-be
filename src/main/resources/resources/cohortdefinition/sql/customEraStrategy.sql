dc1ff83db64adb36e840fa0e7f0d5185874a4079
Chris Knoll <cknoll@ohdsi.org>
2017-11-28 11:29:41 -0500

    Cohort generation optimizations Create inclusion rule temp tables separately. Use UNION ALL for cohort end dates instead of separate cohort temp tables. Removed the ERA_CONSTRUCTOR_TEMPLATE for cohort era-fy logic Split custom drug era and cohort era-fy queries to use temp tables for performance on MPP systems.

--- src/main/resources/resources/cohortdefinition/sql/customEraStrategy.sql
+++ src/main/resources/resources/cohortdefinition/sql/customEraStrategy.sql
@@ -1,4 +1,14 @@
+-- custom era strategy
+
+select de.PERSON_ID, DRUG_EXPOSURE_START_DATE,  COALESCE(DRUG_EXPOSURE_END_DATE, DATEADD(day,DAYS_SUPPLY,DRUG_EXPOSURE_START_DATE), DATEADD(day,1,DRUG_EXPOSURE_START_DATE)) as DRUG_EXPOSURE_END_DATE 
+INTO #drugTarget
+FROM @cdm_database_schema.DRUG_EXPOSURE de
+WHERE de.drug_concept_id in (SELECT concept_id from  #Codesets where codeset_id = @drugCodesetId) 
+	AND de.person_id in (select person_id from @eventTable)
+;
+
 select et.event_id, et.person_id, ERAS.era_end_date as end_date
+INTO #strategy_ends
 from @eventTable et
 JOIN 
 (
@@ -6,65 +16,28 @@ JOIN
   from
   (
     select de.person_id, de.drug_exposure_start_date, MIN(e.END_DATE) as era_end_date
-    FROM (
-      -- cteDrugTarget
-       select de.PERSON_ID, DRUG_EXPOSURE_START_DATE, 
-        COALESCE(DRUG_EXPOSURE_END_DATE, DATEADD(day,DAYS_SUPPLY,DRUG_EXPOSURE_START_DATE), DATEADD(day,1,DRUG_EXPOSURE_START_DATE)) as DRUG_EXPOSURE_END_DATE 
-      FROM @cdm_database_schema.DRUG_EXPOSURE de
-      JOIN @eventTable et on de.PERSON_ID = et.PERSON_ID
-      WHERE de.drug_concept_id in (SELECT concept_id from  #Codesets where codeset_id = @drugCodesetId)
-    ) DE
+    FROM #drugTarget DE
     JOIN 
     (
       --cteEndDates
       select PERSON_ID, DATEADD(day,-1 * @gapDays,EVENT_DATE) as END_DATE -- unpad the end date by @gapDays
       FROM
       (
-        select E1.PERSON_ID, E1.EVENT_DATE, COALESCE(E1.START_ORDINAL,MAX(E2.START_ORDINAL)) START_ORDINAL, E1.OVERALL_ORD 
-        FROM 
-        (
-          select PERSON_ID, EVENT_DATE, EVENT_TYPE, 
-          START_ORDINAL,
-          ROW_NUMBER() OVER (PARTITION BY PERSON_ID ORDER BY EVENT_DATE, EVENT_TYPE) AS OVERALL_ORD -- this re-numbers the inner UNION so all rows are numbered ordered by the event date
-          from
-          (
-            -- select the start dates, assigning a row number to each
-            Select PERSON_ID, DRUG_EXPOSURE_START_DATE AS EVENT_DATE, 0 as EVENT_TYPE, ROW_NUMBER() OVER (PARTITION BY PERSON_ID ORDER BY DRUG_EXPOSURE_START_DATE) as START_ORDINAL
-            from (
-              -- cteDrugTarget
-               select de.PERSON_ID, DRUG_EXPOSURE_START_DATE, 
-                COALESCE(DRUG_EXPOSURE_END_DATE, DATEADD(day,DAYS_SUPPLY,DRUG_EXPOSURE_START_DATE), DATEADD(day,1,DRUG_EXPOSURE_START_DATE)) as DRUG_EXPOSURE_END_DATE 
-              FROM @cdm_database_schema.DRUG_EXPOSURE de
-              JOIN @eventTable et on de.PERSON_ID = et.PERSON_ID
-              WHERE de.drug_concept_id in (SELECT concept_id from  #Codesets where codeset_id = @drugCodesetId)
-            ) D
+				select PERSON_ID, EVENT_DATE, EVENT_TYPE, 
+				MAX(START_ORDINAL) OVER (PARTITION BY PERSON_ID ORDER BY event_date, event_type ROWS UNBOUNDED PRECEDING) AS start_ordinal,
+				ROW_NUMBER() OVER (PARTITION BY PERSON_ID ORDER BY EVENT_DATE, EVENT_TYPE) AS OVERALL_ORD -- this re-numbers the inner UNION so all rows are numbered ordered by the event date
+				from
+				(
+					-- select the start dates, assigning a row number to each
+					Select PERSON_ID, DRUG_EXPOSURE_START_DATE AS EVENT_DATE, 0 as EVENT_TYPE, ROW_NUMBER() OVER (PARTITION BY PERSON_ID ORDER BY DRUG_EXPOSURE_START_DATE) as START_ORDINAL
+					from #drugTarget D
 
-            UNION ALL
+					UNION ALL
 
-            -- add the end dates with NULL as the row number, padding the end dates by @gapDays to allow a grace period for overlapping ranges.
-            select PERSON_ID, DATEADD(day,@gapDays,DRUG_EXPOSURE_END_DATE), 1 as EVENT_TYPE, NULL
-            FROM (
-              -- cteDrugTarget
-               select de.PERSON_ID, DRUG_EXPOSURE_START_DATE, 
-                COALESCE(DRUG_EXPOSURE_END_DATE, DATEADD(day,DAYS_SUPPLY,DRUG_EXPOSURE_START_DATE), DATEADD(day,1,DRUG_EXPOSURE_START_DATE)) as DRUG_EXPOSURE_END_DATE 
-              FROM @cdm_database_schema.DRUG_EXPOSURE de
-              JOIN @eventTable et on de.PERSON_ID = et.PERSON_ID
-              WHERE de.drug_concept_id in (SELECT concept_id from  #Codesets where codeset_id = @drugCodesetId)
-            ) D
-          ) RAWDATA
-        ) E1
-        LEFT JOIN (
-          Select PERSON_ID, DRUG_EXPOSURE_START_DATE AS EVENT_DATE, ROW_NUMBER() OVER (PARTITION BY PERSON_ID ORDER BY DRUG_EXPOSURE_START_DATE) as START_ORDINAL
-          from (
-            -- cteDrugTarget
-             select de.PERSON_ID, DRUG_EXPOSURE_START_DATE, 
-              COALESCE(DRUG_EXPOSURE_END_DATE, DATEADD(day,DAYS_SUPPLY,DRUG_EXPOSURE_START_DATE), DATEADD(day,1,DRUG_EXPOSURE_START_DATE)) as DRUG_EXPOSURE_END_DATE 
-            FROM @cdm_database_schema.DRUG_EXPOSURE de
-            JOIN @eventTable et on de.PERSON_ID = et.PERSON_ID
-            WHERE de.drug_concept_id in (SELECT concept_id from  #Codesets where codeset_id = @drugCodesetId)
-          ) D
-        ) E2 ON E1.PERSON_ID = E2.PERSON_ID AND E2.EVENT_DATE <= E1.EVENT_DATE
-        GROUP BY E1.PERSON_ID, E1.EVENT_DATE, E1.START_ORDINAL, E1.OVERALL_ORD
+					-- add the end dates with NULL as the row number, padding the end dates by @gapDays to allow a grace period for overlapping ranges.
+					select PERSON_ID, DATEADD(day,@gapDays,DRUG_EXPOSURE_END_DATE), 1 as EVENT_TYPE, NULL
+					FROM #drugTarget D
+				) RAWDATA
       ) E
       WHERE 2 * E.START_ORDINAL - E.OVERALL_ORD = 0
     ) E on DE.PERSON_ID = E.PERSON_ID and E.END_DATE >= DE.DRUG_EXPOSURE_START_DATE
@@ -72,5 +45,7 @@ JOIN
   ) ENDS
   GROUP BY ENDS.person_id, ENDS.era_end_date
 ) ERAS on ERAS.person_id = et.person_id 
-WHERE et.start_date between ERAS.era_start_date and ERAS.era_end_date
+WHERE et.start_date between ERAS.era_start_date and ERAS.era_end_date;
 
+TRUNCATE TABLE #drugTarget;
+DROP TABLE #drugTarget;
