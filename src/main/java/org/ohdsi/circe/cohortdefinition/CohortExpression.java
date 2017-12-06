41b65293c1e388663d5368ef18f64f7dccaff8a0
Chris Knoll <cknoll@ohdsi.org>
2017-12-06 13:44:46 -0500

    Add fromJson methods to ConceptsetExpression, CohortExpression and CohortExpressionQueryBuilder.BuildExpressionQueryOptions. Added jackson-databind as dependency. Updated jackson based libraries to 2.8.7 to be inline with other OHDSI libraries (such as WebAPI).

--- src/main/java/org/ohdsi/circe/cohortdefinition/CohortExpression.java
+++ src/main/java/org/ohdsi/circe/cohortdefinition/CohortExpression.java
@@ -20,6 +20,7 @@ package org.ohdsi.circe.cohortdefinition;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,7 +31,8 @@ import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown=true)
 public class CohortExpression {
-  
+  private static final ObjectMapper JSON_MAPPER = new ObjectMapper();
+
   @JsonProperty("Title")  
   public String title;
   
@@ -60,4 +62,13 @@ public class CohortExpression {
   
   @JsonProperty("CollapseSettings")
   public CollapseSettings collapseSettings = new CollapseSettings();
+	
+	public static CohortExpression fromJson(String json) {
+		try {
+			CohortExpression expression = JSON_MAPPER.readValue(json, CohortExpression.class);
+			return expression;
+		} catch (Exception e) {
+			throw new RuntimeException("Error parsing cohort expression", e);
+		}
+	}
 }
