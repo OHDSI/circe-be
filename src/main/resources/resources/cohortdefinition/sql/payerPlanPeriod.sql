d534a6819946726ccd54b71fa34710059faefe01
Pavel Grafkin <pavgra@gmail.com>
2018-02-27 12:04:35 -0500

    Null cast issue (#32)
    * Fixes issue with casting of NULL in Postgres

--- src/main/resources/resources/cohortdefinition/sql/payerPlanPeriod.sql
+++ src/main/resources/resources/cohortdefinition/sql/payerPlanPeriod.sql
@@ -1,5 +1,5 @@
 -- Begin Payer Plan Period Criteria
-select C.person_id, C.payer_plan_period_id as event_id, @startDateExpression as start_date, @endDateExpression as end_date, C.payer_concept_id as TARGET_CONCEPT_ID, NULL as visit_occurrence_id
+select C.person_id, C.payer_plan_period_id as event_id, @startDateExpression as start_date, @endDateExpression as end_date, C.payer_concept_id as TARGET_CONCEPT_ID, CAST(NULL as bigint) as visit_occurrence_id
 from
 (
   select ppp.*, row_number() over (PARTITION BY ppp.person_id ORDER BY ppp.payer_plan_period_start_date) as ordinal
