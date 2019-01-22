ba0c889a407e615656bb8a56152a5e01c62a6d41
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2019-01-22 18:50:01 +0300

    Censor stats query extracted to separate file

--- src/main/resources/resources/cohortdefinition/sql/cohortInclusionAnalysis.sql
+++ src/main/resources/resources/cohortdefinition/sql/cohortInclusionAnalysis.sql
@@ -42,11 +42,3 @@ FROM
   where sr.mode_id = @inclusionImpactMode and sr.cohort_definition_id = @target_cohort_id and sr.inclusion_rule_mask = POWER(cast(2 as bigint),RuleTotal.total_rules)-1
 ) FC
 ;
-
--- calculate censored
-delete from @results_database_schema.cohort_censor_stats where cohort_definition_id = @target_cohort_id;
-insert into @results_database_schema.cohort_censor_stats (cohort_definition_id, lost_count)
-select @target_cohort_id as cohort_definition_id, coalesce(FCC.total_people - TC.total, 0) as lost_count
-FROM
-  (select count_big(distinct person_id) as total_people from #final_cohort) FCC,
-  (select count_big(distinct subject_id) as total from @target_database_schema.@target_cohort_table t where t.cohort_definition_id = @target_cohort_id) TC;
