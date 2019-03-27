95d0b50a62909bbba93f8f9b39ab4b2aba4cdd42
ssuvorov-fls <sergey.suvorov@firstlinesoftware.com>
2019-03-27 15:31:11 +0300

    Allow invalid_reason concepts in results.
    fixes OHDSI/WebAPI#136
    fixes #35

--- src/main/resources/resources/vocabulary/sql/conceptSetQuery.sql
+++ src/main/resources/resources/vocabulary/sql/conceptSetQuery.sql
@@ -1,1 +1,1 @@
-select concept_id from @vocabulary_database_schema.CONCEPT where concept_id in (@conceptIds)and invalid_reason is null
+select concept_id from @vocabulary_database_schema.CONCEPT where concept_id in (@conceptIds)
