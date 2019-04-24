c5198fcac0d661060f54cbb94c0ecb72f96e1ec1
PGrafkin <pavgra@gmail.com>
2019-04-23 21:21:43 -0400

    Extracted ProcedureOccurrenceSqlBuilder

---
+++ src/main/java/org/ohdsi/circe/cohortdefinition/builders/ProcedureOccurrenceSqlBuilder.java
@@ -0,0 +1,117 @@
+package org.ohdsi.circe.cohortdefinition.builders;
+
+import org.apache.commons.lang3.StringUtils;
+import org.ohdsi.circe.cohortdefinition.ProcedureOccurrence;
+import org.ohdsi.circe.helper.ResourceHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.ohdsi.circe.cohortdefinition.builders.BuilderUtils.buildDateRangeClause;
+import static org.ohdsi.circe.cohortdefinition.builders.BuilderUtils.buildNumericRangeClause;
+import static org.ohdsi.circe.cohortdefinition.builders.BuilderUtils.getCodesetJoinExpression;
+import static org.ohdsi.circe.cohortdefinition.builders.BuilderUtils.getConceptIdsFromConcepts;
+
+public class ProcedureOccurrenceSqlBuilder<T extends ProcedureOccurrence> extends BaseCriteriaSqlBuilder<T> {
+
+    private final static String PROCEDURE_OCCURRENCE_TEMPLATE = ResourceHelper.GetResourceAsString("/resources/cohortdefinition/sql/procedureOccurrence.sql");
+
+    @Override
+    protected String getQueryTemplate() {
+
+        return PROCEDURE_OCCURRENCE_TEMPLATE;
+    }
+
+    @Override
+    protected String embedCodesetClause(String query, T criteria) {
+
+        return StringUtils.replace(query, "@codesetClause",
+                getCodesetJoinExpression(criteria.codesetId,
+                        "po.procedure_concept_id",
+                        criteria.procedureSourceConcept,
+                        "po.procedure_source_concept_id")
+        );
+    }
+
+    @Override
+    protected String embedOrdinalExpression(String query, T criteria, List<String> whereClauses) {
+
+        // first
+        if (criteria.first != null && criteria.first) {
+            whereClauses.add("C.ordinal = 1");
+            query = StringUtils.replace(query, "@ordinalExpression", ", row_number() over (PARTITION BY po.person_id ORDER BY po.procedure_date, po.procedure_occurrence_id) as ordinal");
+        } else {
+            query = StringUtils.replace(query, "@ordinalExpression", "");
+        }
+        return query;
+    }
+
+    @Override
+    protected List<String> resolveJoinClauses(T criteria) {
+
+        List<String> joinClauses = new ArrayList<>();
+
+        // join to PERSON
+        if (criteria.age != null || (criteria.gender != null && criteria.gender.length > 0)) {
+            joinClauses.add("JOIN @cdm_database_schema.PERSON P on C.person_id = P.person_id");
+        }
+        if (criteria.visitType != null && criteria.visitType.length > 0) {
+            joinClauses.add("JOIN @cdm_database_schema.VISIT_OCCURRENCE V on C.visit_occurrence_id = V.visit_occurrence_id and C.person_id = V.person_id");
+        }
+        if (criteria.providerSpecialty != null && criteria.providerSpecialty.length > 0) {
+            joinClauses.add("LEFT JOIN @cdm_database_schema.PROVIDER PR on C.provider_id = PR.provider_id");
+        }
+
+        return joinClauses;
+    }
+
+    @Override
+    protected List<String> resolveWhereClauses(T criteria) {
+
+        List<String> whereClauses = new ArrayList<>();
+
+        // occurrenceStartDate
+        if (criteria.occurrenceStartDate != null) {
+            whereClauses.add(buildDateRangeClause("C.procedure_date", criteria.occurrenceStartDate));
+        }
+
+        // procedureType
+        if (criteria.procedureType != null && criteria.procedureType.length > 0) {
+            ArrayList<Long> conceptIds = getConceptIdsFromConcepts(criteria.procedureType);
+            whereClauses.add(String.format("C.procedure_type_concept_id %s in (%s)", (criteria.procedureTypeExclude ? "not" : ""), StringUtils.join(conceptIds, ",")));
+        }
+
+        // modifier
+        if (criteria.modifier != null && criteria.modifier.length > 0) {
+            ArrayList<Long> conceptIds = getConceptIdsFromConcepts(criteria.modifier);
+            whereClauses.add(String.format("C.modifier_concept_id in (%s)", StringUtils.join(conceptIds, ",")));
+        }
+
+        // quantity
+        if (criteria.quantity != null) {
+            whereClauses.add(buildNumericRangeClause("C.quantity", criteria.quantity));
+        }
+
+        // age
+        if (criteria.age != null) {
+            whereClauses.add(buildNumericRangeClause("YEAR(C.procedure_date) - P.year_of_birth", criteria.age));
+        }
+
+        // gender
+        if (criteria.gender != null && criteria.gender.length > 0) {
+            whereClauses.add(String.format("P.gender_concept_id in (%s)", StringUtils.join(getConceptIdsFromConcepts(criteria.gender), ",")));
+        }
+
+        // providerSpecialty
+        if (criteria.providerSpecialty != null && criteria.providerSpecialty.length > 0) {
+            whereClauses.add(String.format("PR.specialty_concept_id in (%s)", StringUtils.join(getConceptIdsFromConcepts(criteria.providerSpecialty), ",")));
+        }
+
+        // visitType
+        if (criteria.visitType != null && criteria.visitType.length > 0) {
+            whereClauses.add(String.format("V.visit_concept_id in (%s)", StringUtils.join(getConceptIdsFromConcepts(criteria.visitType), ",")));
+        }
+
+        return whereClauses;
+    }
+}
