ee2b896b5ba4a1a3e2efbbdbad9ba3f0ca263a3d
PGrafkin <pavgra@gmail.com>
2019-04-23 22:18:59 -0400

    Extracted ConditionEraSqlBuilder

--- src/main/java/org/ohdsi/circe/cohortdefinition/CohortExpressionQueryBuilder.java
+++ src/main/java/org/ohdsi/circe/cohortdefinition/CohortExpressionQueryBuilder.java
@@ -25,6 +25,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import org.apache.commons.lang3.StringUtils;
 import org.ohdsi.circe.cohortdefinition.builders.BaseCriteriaSqlBuilder;
+import org.ohdsi.circe.cohortdefinition.builders.ConditionEraSqlBuilder;
 import org.ohdsi.circe.cohortdefinition.builders.ConditionOccurrenceSqlBuilder;
 import org.ohdsi.circe.cohortdefinition.builders.DeathSqlBuilder;
 import org.ohdsi.circe.cohortdefinition.builders.DeviceExposureSqlBuilder;
@@ -66,7 +67,6 @@ public class CohortExpressionQueryBuilder implements IGetCriteriaSqlDispatcher, 
   private final static String ADDITIONAL_CRITERIA_TEMPLATE = StringUtils.replace(ResourceHelper.GetResourceAsString("/resources/cohortdefinition/sql/additionalCriteria.sql"), "@windowedCriteria", WINDOWED_CRITERIA_TEMPLATE);
   private final static String GROUP_QUERY_TEMPLATE = ResourceHelper.GetResourceAsString("/resources/cohortdefinition/sql/groupQuery.sql");
 
-  private final static String CONDITION_ERA_TEMPLATE = ResourceHelper.GetResourceAsString("/resources/cohortdefinition/sql/conditionEra.sql");
   private final static String PRIMARY_CRITERIA_EVENTS_TABLE = "primary_events";
   private final static String INCLUSION_RULE_QUERY_TEMPLATE = ResourceHelper.GetResourceAsString("/resources/cohortdefinition/sql/inclusionrule.sql");
   private final static String CENSORING_QUERY_TEMPLATE = ResourceHelper.GetResourceAsString("/resources/cohortdefinition/sql/censoringInsert.sql");
@@ -611,88 +611,8 @@ public class CohortExpressionQueryBuilder implements IGetCriteriaSqlDispatcher, 
 // <editor-fold defaultstate="collapsed" desc="ICriteriaSqlDispatcher implementation">
 
   @Override
-  public String getCriteriaSql(ConditionEra criteria)
-  {
-    String query = CONDITION_ERA_TEMPLATE;
-
-    String codesetClause = "";
-    if (criteria.codesetId != null)
-    {
-      codesetClause = String.format("where ce.condition_concept_id in (SELECT concept_id from  #Codesets where codeset_id = %d)", criteria.codesetId);
-    }
-    query = StringUtils.replace(query, "@codesetClause",codesetClause);
-
-    ArrayList<String> joinClauses = new ArrayList<>();
-
-    if (criteria.ageAtStart != null || criteria.ageAtEnd != null || (criteria.gender != null && criteria.gender.length > 0)) // join to PERSON
-      joinClauses.add("JOIN @cdm_database_schema.PERSON P on C.person_id = P.person_id");
-
-    query = StringUtils.replace(query,"@joinClause", StringUtils.join(joinClauses,"\n"));
-
-    ArrayList<String> whereClauses = new ArrayList<>();
-
-		// first
-		if (criteria.first != null && criteria.first == true) {
-			whereClauses.add("C.ordinal = 1");
-			query = StringUtils.replace(query, "@ordinalExpression", ", row_number() over (PARTITION BY ce.person_id ORDER BY ce.condition_era_start_date, ce.condition_era_id) as ordinal");
-		}
-		else {
-			query = StringUtils.replace(query, "@ordinalExpression","");
-		}
-
-    // eraStartDate
-    if (criteria.eraStartDate != null)
-    {
-      whereClauses.add(buildDateRangeClause("C.condition_era_start_date",criteria.eraStartDate));
-    }
-
-    // eraEndDate
-    if (criteria.eraEndDate != null)
-    {
-      whereClauses.add(buildDateRangeClause("C.condition_era_end_date",criteria.eraEndDate));
-    }
-
-    // occurrenceCount
-    if (criteria.occurrenceCount != null)
-    {
-      whereClauses.add(buildNumericRangeClause("C.condition_occurrence_count", criteria.occurrenceCount));
-    }
-
-    // eraLength
-    if (criteria.eraLength != null)
-    {
-      whereClauses.add(buildNumericRangeClause("DATEDIFF(d,C.condition_era_start_date, C.condition_era_end_date)", criteria.eraLength));
-    }
-
-    // ageAtStart
-    if (criteria.ageAtStart != null)
-    {
-      whereClauses.add(buildNumericRangeClause("YEAR(C.condition_era_start_date) - P.year_of_birth", criteria.ageAtStart));
-    }
-
-    // ageAtEnd
-    if (criteria.ageAtEnd != null)
-    {
-      whereClauses.add(buildNumericRangeClause("YEAR(C.condition_era_end_date) - P.year_of_birth", criteria.ageAtEnd));
-    }
-
-    // gender
-    if (criteria.gender != null && criteria.gender.length > 0)
-    {
-      whereClauses.add(String.format("P.gender_concept_id in (%s)", StringUtils.join(getConceptIdsFromConcepts(criteria.gender),",")));
-    }
-
-    String whereClause = "";
-    if (whereClauses.size() > 0)
-      whereClause = "WHERE " + StringUtils.join(whereClauses, "\nAND ");
-    query = StringUtils.replace(query, "@whereClause",whereClause);
-
-    if (criteria.CorrelatedCriteria != null && !criteria.CorrelatedCriteria.isEmpty())
-    {
-      query = wrapCriteriaQuery(query, criteria.CorrelatedCriteria);
-    }
-
-    return query;
+  public String getCriteriaSql(ConditionEra criteria) {
+    return getCriteriaSql(new ConditionEraSqlBuilder<>(), criteria);
   }
 
   @Override
