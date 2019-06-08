d4fb56b4fdccb46b8ff0e35b63a1e9fd1c5993fd
PGrafkin <pavgra@gmail.com>
2019-06-08 14:10:05 -0400

    Updated date assembly

--- src/main/resources/resources/cohortdefinition/sql/locationRegion.sql
+++ src/main/resources/resources/cohortdefinition/sql/locationRegion.sql
@@ -3,7 +3,7 @@ select
   C.entity_id AS person_id,
   C.location_history_id as event_id,
   C.start_date as start_date,
-  ISNULL(C.end_date, CAST('20991231' AS DATE)) as end_date,
+  ISNULL(C.end_date, DATEFROMPARTS(2099,12,31)) as end_date,
   C.region_concept_id as TARGET_CONCEPT_ID,
   CAST(NULL as bigint) as visit_occurrence_id
 from 
