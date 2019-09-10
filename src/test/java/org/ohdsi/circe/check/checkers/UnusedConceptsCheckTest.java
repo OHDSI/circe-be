fb9e5f627b8a7e4c6d034a0000d0dab6e2597503
molodkov yaroslav <yaroslav.molodkov@odysseusinc.com>
2019-09-10 20:58:42 +0200

    Issue 1933 atlas cohort check incorrectly flags concept set as unused (#90)
    Cohort check incorrectly flags concept set as unused when it is part of the definition #1250
    - add unittest

---
+++ src/test/java/org/ohdsi/circe/check/checkers/UnusedConceptsCheckTest.java
@@ -0,0 +1,27 @@
+package org.ohdsi.circe.check.checkers;
+
+import static org.junit.Assert.*;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import org.junit.Test;
+import org.ohdsi.circe.check.Warning;
+import org.ohdsi.circe.cohortdefinition.CohortExpression;
+import org.ohdsi.circe.helper.ResourceHelper;
+
+public class UnusedConceptsCheckTest {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    private UnusedConceptsCheck unusedConceptsCheck = new UnusedConceptsCheck();
+
+
+    @Test
+    public void check_usageInTheChildGroups() throws IOException {
+        CohortExpression cohortExpression = CohortExpression.fromJson(ResourceHelper.GetResourceAsString("/checkers/childGroupExpression.json"));
+        List<Warning> warnings = unusedConceptsCheck.check(cohortExpression);
+        assertEquals(Collections.emptyList(), warnings);
+    }
+}
