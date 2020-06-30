0e675b0deb85ebf3970e1fbc80cdfbd22a5f1c45
Benjamin Amor <bamor@palantir.com>
2020-06-30 16:13:10 -0700

    fixing capitalization again

--- src/main/resources/resources/cohortdefinition/sql/censoringInsert.sql
+++ src/main/resources/resources/cohortdefinition/sql/censoringInsert.sql
@@ -3,5 +3,5 @@ FROM included_events i
 JOIN
 (
 @criteriaQuery
-) C on C.person_id = i.person_id and C.start_date >= i.start_date and C.START_DATE <= i.op_end_date
+) C on C.person_id = i.person_id and C.start_date >= i.start_date and C.start_date <= i.op_end_date
 GROUP BY i.event_id, i.person_id
