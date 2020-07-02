cc904a6a40299027f3661174a22d1dee4dd5e0f3
Chris Knoll <cknoll@ohdsi.org>
2020-07-02 11:12:46 -0400

    Increased code coverage of concept set expressions Formatted code to follow two-space indentation.

--- src/main/java/org/ohdsi/circe/vocabulary/ConceptSetExpression.java
+++ src/main/java/org/ohdsi/circe/vocabulary/ConceptSetExpression.java
@@ -19,6 +19,9 @@
 package org.ohdsi.circe.vocabulary;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.ohdsi.analysis.Utils;
+
 import java.util.Arrays;
 import java.util.Objects;
 
@@ -27,60 +30,55 @@ import java.util.Objects;
  * A Class that encapsulates the elements of a Concept Set Expression.
  */
 public class ConceptSetExpression {
-	
+
   private static final ObjectMapper JSON_MAPPER = new ObjectMapper();
-	
-  public static class ConceptSetItem
-  {
+
+  public static class ConceptSetItem {
     public Concept concept;
     public boolean isExcluded;
     public boolean includeDescendants;
     public boolean includeMapped;
 
-      @Override
-      public boolean equals(Object o) {
-          if (this == o) {
-              return true;
-          }
-          if (!(o instanceof ConceptSetItem)) {
-              return false;
-          }
-          ConceptSetItem other = (ConceptSetItem) o;
-          return Objects.equals(concept, other.concept) && Objects.equals(isExcluded, other.isExcluded) && Objects.equals(includeDescendants, other.includeDescendants) &&
-                  Objects.equals(includeMapped, other.includeMapped);
-      }
-
-      @Override
-      public int hashCode() {
-          return Objects.hash(concept, isExcluded, includeDescendants, includeMapped);
-      }
-  }
-
-  public ConceptSetItem[] items;
-  
-	public ConceptSetExpression fromJson(String json) {
-		try {
-			ConceptSetExpression expression = JSON_MAPPER.readValue(json, ConceptSetExpression.class);
-			return expression;
-		} catch (Exception e) {
-			throw new RuntimeException("Error parsing conceptset expression", e);
-		}
-	}
-
     @Override
     public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof ConceptSetExpression)) {
-            return false;
-        }
-        ConceptSetExpression other = (ConceptSetExpression) o;
-        return Arrays.equals(items, other.items);
+      if (this == o) {
+        return true;
+      }
+      if (!(o instanceof ConceptSetItem)) {
+        return false;
+      }
+      ConceptSetItem other = (ConceptSetItem) o;
+      return Objects.equals(concept, other.concept) && Objects.equals(isExcluded, other.isExcluded)
+          && Objects.equals(includeDescendants, other.includeDescendants)
+          && Objects.equals(includeMapped, other.includeMapped);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(items);
+      return Objects.hash(concept, isExcluded, includeDescendants, includeMapped);
     }
+  }
+
+  public ConceptSetItem[] items;
+
+  public static ConceptSetExpression fromJson(String json) {
+    return Utils.deserialize(json, ConceptSetExpression.class);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof ConceptSetExpression)) {
+      return false;
+    }
+    ConceptSetExpression other = (ConceptSetExpression) o;
+    return Arrays.equals(items, other.items);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(items);
+  }
 }
--- src/test/java/org/ohdsi/circe/conceptset/ConceptSetTest.java
+++ src/test/java/org/ohdsi/circe/conceptset/ConceptSetTest.java
@@ -7,83 +7,118 @@ import org.ohdsi.circe.BaseTest;
 import org.ohdsi.circe.cohortdefinition.ConceptSet;
 import org.ohdsi.circe.vocabulary.ConceptSetExpression;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 public class ConceptSetTest extends BaseTest {
 
-    @Test
-    public void testEqualsSameExpression() throws IOException {
+  @Test
+  public void testEqualsSameExpression() throws IOException {
 
-        //Arrange
-        String csExpression = readResource("/conceptset/dupixentExpression.json");
-        ConceptSetExpression firstExpression = Utils.deserialize(csExpression, ConceptSetExpression.class);
-        ConceptSetExpression secondExpression = Utils.deserialize(csExpression, ConceptSetExpression.class);
+    // Arrange
+    String csExpression = readResource("/conceptset/dupixentExpression.json");
+    ConceptSetExpression firstExpression = Utils.deserialize(csExpression, ConceptSetExpression.class);
+    ConceptSetExpression secondExpression = Utils.deserialize(csExpression, ConceptSetExpression.class);
 
-        ConceptSet firstCs = new ConceptSet();
-        firstCs.expression = firstExpression;
-        firstCs.id = 1;
+    ConceptSet firstCs = new ConceptSet();
+    firstCs.expression = firstExpression;
+    firstCs.id = 1;
 
-        ConceptSet secondCs = new ConceptSet();
-        secondCs.expression = secondExpression;
-        secondCs.id = 1;
+    ConceptSet secondCs = new ConceptSet();
+    secondCs.expression = secondExpression;
+    secondCs.id = 1;
 
-        //Assert
-        assertTrue(firstCs.equals(secondCs));
-    }
-    
-    @Test
-    public void testEqualsDifferentExpressions() throws IOException {
+    // Assert
+    assertTrue(firstCs.equals(secondCs));
+    assertTrue(firstCs.equals(firstCs)); // compare same reference
+    assertEquals(firstCs.hashCode(), secondCs.hashCode()); // check hashcode
+    // expression checks
+    assertTrue(firstExpression.equals(firstExpression)); // self check
+    assertTrue(firstExpression.equals(secondExpression));
+    assertTrue(firstExpression.items[0].equals(secondExpression.items[0])); // compare first concept set item
+    assertTrue(firstExpression.items[0].equals(firstExpression.items[0])); // compare first concept set item same reference
+    assertEquals(firstExpression.hashCode(), secondExpression.hashCode());
 
-        //Arrange
-        String dupixentExpression = readResource("/conceptset/dupixentExpression.json");
-        ConceptSetExpression firstExpression = Utils.deserialize(dupixentExpression, ConceptSetExpression.class);
+    // concept Check
+    assertTrue(firstExpression.items[0].concept.equals(firstExpression.items[0].concept));
 
-        String dupilumabExpression = readResource("/conceptset/dupilumabExpression.json");
-        ConceptSetExpression secondExpression = Utils.deserialize(dupilumabExpression, ConceptSetExpression.class);
+  }
 
-        ConceptSet firstCs = new ConceptSet();
-        firstCs.expression = firstExpression;
-        firstCs.id = 1;
+  @Test
+  public void testEqualsDifferentExpressions() throws IOException {
 
-        ConceptSet secondCs = new ConceptSet();
-        secondCs.expression = secondExpression;
-        secondCs.id = 1;
+    // Arrange
+    String dupixentExpression = readResource("/conceptset/dupixentExpression.json");
+    ConceptSetExpression firstExpression = ConceptSetExpression.fromJson(dupixentExpression);
 
-        //Assert
-        assertFalse(firstCs.equals(secondCs));
-    }
-    
-    @Test
-    public void testEqualsDifferentClasses() throws IOException {
+    String dupilumabExpression = readResource("/conceptset/dupilumabExpression.json");
+    ConceptSetExpression secondExpression =  ConceptSetExpression.fromJson(dupilumabExpression);
 
-        //Arrange
-        String csExpression = readResource("/conceptset/dupixentExpression.json");
-        ConceptSetExpression expression = Utils.deserialize(csExpression, ConceptSetExpression.class);
+    ConceptSet firstCs = new ConceptSet();
+    firstCs.expression = firstExpression;
+    firstCs.id = 1;
 
-        ConceptSet conceptSet = new ConceptSet();
-        conceptSet.expression = expression;
-        conceptSet.id = 1;
-        
-        //Assert
-        assertFalse(conceptSet.equals(expression));       
-    }
+    ConceptSet secondCs = new ConceptSet();
+    secondCs.expression = secondExpression;
+    secondCs.id = 2;
 
-    @Test
-    public void testEqualsWithNullObject() throws IOException {
+    // Assert
+    assertFalse(firstCs.equals(secondCs));
+    assertFalse(firstCs.equals(null)); // check null
+    // reset ids to match, but still should be non-equal
+    secondCs.id = firstCs.id;
+    assertFalse(firstCs.equals(secondCs));
 
-        //Arrange
-        String csExpression = readResource("/conceptset/dupixentExpression.json");
-        ConceptSetExpression expression = Utils.deserialize(csExpression, ConceptSetExpression.class);
+    // expression checks
+    assertFalse(firstExpression.equals(secondExpression));
+    assertFalse(firstExpression.equals(new Object())); // check different object type
+    assertFalse(firstExpression.equals(null)); // check null
+    assertNotEquals(firstExpression.hashCode(), secondExpression.hashCode());
 
-        ConceptSet conceptSet = new ConceptSet();
-        conceptSet.expression = expression;
-        conceptSet.id = 1;
-        
-        expression = null;
+    // conceptSet Item checks
+    assertFalse(firstExpression.items[0].equals(secondExpression.items[0])); // compare first concept set item
+    assertFalse(firstExpression.items[0].equals(new Object())); // check different objects
+    assertFalse(firstExpression.items[0].equals(null)); // check null 
 
-        //Assert
-        assertFalse(conceptSet.equals(expression));
-    }
+    // concept Check
+    assertFalse(firstExpression.items[0].concept.equals(secondExpression.items[0].concept));
+    assertFalse(firstExpression.items[0].concept.equals(new Object()));
+    assertFalse(firstExpression.items[0].concept.equals(null));
+
+  }
+
+  @Test
+  public void testEqualsDifferentClasses() throws IOException {
+
+    // Arrange
+    String csExpression = readResource("/conceptset/dupixentExpression.json");
+    ConceptSetExpression expression = Utils.deserialize(csExpression, ConceptSetExpression.class);
+
+    ConceptSet conceptSet = new ConceptSet();
+    conceptSet.expression = expression;
+    conceptSet.id = 1;
+
+    // Assert
+    assertFalse(conceptSet.equals(expression));
+  }
+
+  @Test
+  public void testEqualsWithNullObject() throws IOException {
+
+    // Arrange
+    String csExpression = readResource("/conceptset/dupixentExpression.json");
+    ConceptSetExpression expression = Utils.deserialize(csExpression, ConceptSetExpression.class);
+
+    ConceptSet conceptSet = new ConceptSet();
+    conceptSet.expression = expression;
+    conceptSet.id = 1;
+
+    expression = null;
+
+    // Assert
+    assertFalse(conceptSet.equals(expression));
+  }
 
 }
