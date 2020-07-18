750938a86d9b1fec1e96ee27848caa2e57a8270e
Benjamin Amor <bamor@palantir.com>
2020-07-18 06:51:40 -0700

    another capitalization issue

--- src/main/resources/resources/cohortdefinition/sql/demographicCriteria.sql
+++ src/main/resources/resources/cohortdefinition/sql/demographicCriteria.sql
@@ -1,5 +1,5 @@
 -- Begin Demographic Criteria
-SELECT @indexId as index_id, E.person_id, e.event_id
+SELECT @indexId as index_id, E.person_id, E.event_id
 FROM @eventTable E
 JOIN global_temp.person P ON P.person_id = E.person_id
 @whereClause
