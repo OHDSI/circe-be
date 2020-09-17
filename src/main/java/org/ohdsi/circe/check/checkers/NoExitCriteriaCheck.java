c5a79dc0e0da929da49a9b25f031ca85f4446a2b
Sergey Suvorov <sergey.suvorov@odysseusinc.com>
2020-09-17 09:00:34 +0300

    fixed indentation (#129)

--- src/main/java/org/ohdsi/circe/check/checkers/NoExitCriteriaCheck.java
+++ src/main/java/org/ohdsi/circe/check/checkers/NoExitCriteriaCheck.java
@@ -4,7 +4,7 @@
  *   you may not use this file except in compliance with the License.
  *   You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  *   Unless required by applicable law or agreed to in writing, software
  *   distributed under the License is distributed on an "AS IS" BASIS,
@@ -27,22 +27,22 @@ import org.ohdsi.circe.cohortdefinition.CohortExpression;
 
 public class NoExitCriteriaCheck extends BaseCheck {
 
-    private static final String NO_EXIT_CRITERIA_WARNING = " \"all events\" are selected and cohort exit criteria has not been specified";
+  private static final String NO_EXIT_CRITERIA_WARNING = " \"all events\" are selected and cohort exit criteria has not been specified";
 
-    @Override
-    protected WarningSeverity defineSeverity() {
+  @Override
+  protected WarningSeverity defineSeverity() {
 
-        return WarningSeverity.WARNING;
-    }
+    return WarningSeverity.WARNING;
+  }
 
-    @Override
-    protected void check(CohortExpression expression, WarningReporter reporter) {
-        match(expression)
-                .when(e -> Objects.equals("All", e.primaryCriteria.primaryLimit.type)
-                        && Objects.isNull(e.endStrategy)
-                        && Objects.equals("All", e.expressionLimit.type)
-                        && (Objects.isNull(e.additionalCriteria)
-                            || Objects.equals("All", e.qualifiedLimit.type)))
-                .then(() -> reporter.add(NO_EXIT_CRITERIA_WARNING));
-    }
+  @Override
+  protected void check(CohortExpression expression, WarningReporter reporter) {
+    match(expression)
+        .when(e -> "All".equalsIgnoreCase(e.primaryCriteria.primaryLimit.type)
+            && Objects.isNull(e.endStrategy)
+            && "All".equalsIgnoreCase(e.expressionLimit.type)
+            && (Objects.isNull(e.additionalCriteria)
+              || "All".equalsIgnoreCase(e.qualifiedLimit.type)))
+        .then(() -> reporter.add(NO_EXIT_CRITERIA_WARNING));
+  }
 }
