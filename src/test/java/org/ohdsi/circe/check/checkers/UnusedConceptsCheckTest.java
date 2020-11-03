4233066b9ea058c98215ee39c4cf009527ae5e6b
Sergey Suvorov <sergey.suvorov@odysseusinc.com>
2020-11-03 10:30:30 +0300

    added tests (#129)

--- src/test/java/org/ohdsi/circe/check/checkers/UnusedConceptsCheckTest.java
+++ src/test/java/org/ohdsi/circe/check/checkers/UnusedConceptsCheckTest.java
@@ -7,10 +7,16 @@ import java.util.Collections;
 import java.util.List;
 import org.junit.Test;
 import org.ohdsi.circe.check.Warning;
+import org.ohdsi.circe.check.warnings.ConceptSetWarning;
 import org.ohdsi.circe.cohortdefinition.CohortExpression;
 import org.ohdsi.circe.helper.ResourceHelper;
 
 public class UnusedConceptsCheckTest {
+    private static final CohortExpression UNUSED_CONCEPT_EXPRESSION =
+            CohortExpression.fromJson(ResourceHelper.GetResourceAsString("/checkers/unusedConceptSet.json"));
+    private static final CohortExpression UNUSED_CONCEPT_CORRECT_EXPRESSION =
+            CohortExpression.fromJson(ResourceHelper.GetResourceAsString("/checkers/unusedConceptSetCorrect.json"));
+    private static final long UNUSED_CONCEPT_COUNT = 13;
 
     private UnusedConceptsCheck unusedConceptsCheck = new UnusedConceptsCheck();
 
@@ -19,5 +25,21 @@ public class UnusedConceptsCheckTest {
         CohortExpression cohortExpression = CohortExpression.fromJson(ResourceHelper.GetResourceAsString("/checkers/childGroupExpression.json"));
         List<Warning> warnings = unusedConceptsCheck.check(cohortExpression);
         assertEquals(Collections.emptyList(), warnings);
+    }
+
+    @Test
+    public void checkUnusedConceptSetCorrect() {
+        List<Warning> warnings = unusedConceptsCheck.check(UNUSED_CONCEPT_CORRECT_EXPRESSION);
+        warnings.forEach((warning) -> assertNotEquals(ConceptSetWarning.class, warning.getClass()));
+    }
+
+    @Test
+    public void checkUnusedConceptSet() {
+        List<Warning> warnings = unusedConceptsCheck.check(UNUSED_CONCEPT_EXPRESSION);
+        long count = warnings
+                .stream()
+                .filter(warning -> warning instanceof ConceptSetWarning)
+                .count();
+        assertEquals(UNUSED_CONCEPT_COUNT, count);
     }
 }
