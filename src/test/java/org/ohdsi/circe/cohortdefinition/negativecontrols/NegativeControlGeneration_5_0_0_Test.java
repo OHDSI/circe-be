1d538eb48a069490141914cdaba1ff3cd0e3351e
Anthony Sena <asena5@its.jnj.com>
2020-12-11 13:25:11 -0500

    Add first tests

--- src/test/java/org/ohdsi/circe/cohortdefinition/negativecontrols/NegativeControlGeneration_5_0_0_Test.java
+++ src/test/java/org/ohdsi/circe/cohortdefinition/negativecontrols/NegativeControlGeneration_5_0_0_Test.java
@@ -16,19 +16,19 @@
 package org.ohdsi.circe.cohortdefinition.negativecontrols;
 
 import com.github.mjeanroy.dbunit.core.dataset.DataSetFactory;
-import java.sql.SQLException;
+import java.util.List;
 import org.dbunit.Assertion;
-import org.dbunit.DatabaseUnitException;
 import org.dbunit.database.IDatabaseConnection;
-import org.dbunit.dataset.DataSetException;
 import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.ITable;
 import org.dbunit.operation.DatabaseOperation;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertTrue;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.ohdsi.circe.AbstractDatabaseTest;
+import org.ohdsi.sql.SqlRender;
+import org.ohdsi.sql.SqlSplit;
+import org.ohdsi.sql.SqlTranslate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -47,16 +47,30 @@ public class NegativeControlGeneration_5_0_0_Test extends AbstractDatabaseTest {
     jdbcTemplate = new JdbcTemplate(getDataSource());
     prepareSchema("cdm", CDM_DDL_PATH);
   }
-
+  
   @Test
-  public void defaultTest() throws Exception {
-    final String RESULTS_SCHEMA = "someNegativeControlTest";
+  public void buildEmptyExpressionQuery() throws Exception {
+    boolean exceptionThrown = false;
+    CohortExpressionQueryBuilder queryBuilder = new CohortExpressionQueryBuilder();
+    OutcomeCohortExpression cohortExpression = new OutcomeCohortExpression();
+    try {
+      String cohortSql = queryBuilder.buildExpressionQuery(cohortExpression);
+      } catch (Exception re)
+    {
+      exceptionThrown = true;
+    }
+    assertTrue("A NULL cohort expression results in an exception.", exceptionThrown);
+  }
+  
+  @Test
+  public void firstOccurrenceTest() throws Exception {
+    final String RESULTS_SCHEMA = "negFirstOccurrenceTest";
     final String[] testDataSetsPrep = new String[] { 
       "/datasets/vocabulary.json",
-      "/negativecontrols/defaultTest/someTest_PREP.json" 
+      "/negativecontrols/firstOccurrence/firstOccurrenceTest_PREP.json" 
     };
 
-    final String[] testDataSetsVerify = new String[] {"negativecontrols/defaultTest/someTest_VERIFY.json"};
+    final String[] testDataSetsVerify = new String[] {"/negativecontrols/firstOccurrence/firstOccurrenceTest_VERIFY.json"};
     final IDatabaseConnection dbUnitCon = getConnection();
 
     // prepare results schema for the specified options.resultSchema
@@ -65,6 +79,17 @@ public class NegativeControlGeneration_5_0_0_Test extends AbstractDatabaseTest {
     // load test data into DB.
     final IDataSet dsPrep = DataSetFactory.createDataSet(testDataSetsPrep);
     DatabaseOperation.CLEAN_INSERT.execute(dbUnitCon, dsPrep); // clean load of the DB. Careful, clean means "delete the old stuff"
+    
+    // Perform test
+    CohortExpressionQueryBuilder queryBuilder = new CohortExpressionQueryBuilder();
+    OutcomeCohortExpression cohortExpression = new OutcomeCohortExpression();
+    cohortExpression.occurrenceType = OccurrenceType.FIRST;
+    cohortExpression.domains.add("condition");
+    cohortExpression.detectOnDescendants = true;
+    String sql = buildExpressionSql(cohortExpression, RESULTS_SCHEMA, "1"); // 1 == ancestor concept id
+    
+    // execute on database, expect no errors
+    jdbcTemplate.batchUpdate(SqlSplit.splitSql(sql));
 
     // Validate results
     // Load actual records from cohort table
@@ -76,5 +101,20 @@ public class NegativeControlGeneration_5_0_0_Test extends AbstractDatabaseTest {
     // Assert actual database table match expected table
     Assertion.assertEquals(expectedTable, actualTable);
   }
+  
+  private static String buildExpressionSql(final OutcomeCohortExpression expression,
+      String targetDatabaseSchema, String outcomeId) throws Exception {
+    // build SQL
+    final CohortExpressionQueryBuilder builder = new CohortExpressionQueryBuilder();
+    String cohortSql = builder.buildExpressionQuery(expression);
+
+    // translate to PG
+    String[] parameters = new String[] {"outcome_ids", "cdm_database_schema", "target_database_schema", "target_cohort_table"};
+    String[] values = new String[] { outcomeId, "cdm", targetDatabaseSchema, "cohort" };
+    cohortSql = SqlRender.renderSql(SqlTranslate.translateSql(cohortSql, "postgresql"), parameters, values);
+
+    return cohortSql;
+  }
+  
 }
 
