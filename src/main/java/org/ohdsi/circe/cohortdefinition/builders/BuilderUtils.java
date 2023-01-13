a20559f7590acff612e3b33d1717a61d849ded3e
Anthony Sena <asena5@its.jnj.com>
2023-01-13 10:08:07 -0500

    Forces decimal formatting to Locale.US - fixes #184

--- src/main/java/org/ohdsi/circe/cohortdefinition/builders/BuilderUtils.java
+++ src/main/java/org/ohdsi/circe/cohortdefinition/builders/BuilderUtils.java
@@ -6,6 +6,7 @@ import org.ohdsi.circe.cohortdefinition.NumericRange;
 import org.ohdsi.circe.cohortdefinition.TextFilter;
 import org.ohdsi.circe.vocabulary.Concept;
 
+import java.util.Locale;
 import java.util.ArrayList;
 
 public abstract class BuilderUtils {
@@ -94,14 +95,14 @@ public abstract class BuilderUtils {
 
         String clause;
         if (range.op.endsWith("bt")) {
-            clause = String.format("%s(%s >= %" + format + " and %s <= %" + format + ")",
+            clause = String.format("%s(%s >= %s and %s <= %s)",
                     range.op.startsWith("!") ? "not " : "",
                     sqlExpression,
-                    range.value.doubleValue(),
+                    formatDouble(range.value.doubleValue(), format),
                     sqlExpression,
-                    range.extent.doubleValue());
+                    formatDouble(range.extent.doubleValue(), format));
         } else {
-            clause = String.format("%s %s %" + format, sqlExpression, getOperator(range), range.value.doubleValue());
+            clause = String.format("%s %s %s", sqlExpression, getOperator(range), formatDouble(range.value.doubleValue(), format));
         }
         return clause;
     }
@@ -148,5 +149,12 @@ public abstract class BuilderUtils {
             return value;
         }
         return value.replaceAll("\\\\*\\'", "''");
+    }
+    
+    private static String formatDouble(double d, String format) {
+        // Forces the US Locale formatting for all double values
+        // for Issue #184: https://github.com/OHDSI/circe-be/issues/184
+        String formatString = "%" + format;
+        return String.format(Locale.US, formatString, d);
     }
 }
