005ae0f09617c82ab0c20a921d8947736794de4e
Alex Manoylenko <alexey.manoylenko@odysseusinc.com>
2024-05-01 13:31:10 +0000

    When building the cohort_details join by concept_id doesn't look correct changing to join by person_id and event_id instead

--- src/main/resources/resources/cohortdefinition/sql/generateCohort.sql
+++ src/main/resources/resources/cohortdefinition/sql/generateCohort.sql
@@ -124,7 +124,7 @@ select qe.op_start_date, qe.op_end_date, qe.visit_occurrence_id,
       @strategy_ends_columns
 into #final_cohort_details
 from #qualified_events qe
-left join inclusion_events ie on qe.concept_id = ie.concept_id
+left join inclusion_events ie on qe.person_id = ie.person_id AND qe.event_id = ie.event_id
 @leftjoinEraStrategy
 ;
 
